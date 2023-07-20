d0f0842f16a3128b65188b95afdc2a28377db82c
AI-Jason-Young <AI.Jason.Young@outlook.com>
2023-07-20 16:40:16 +0800

    Update

--- Experiments/DETR/PyTorch/build_env.sh
+++ Experiments/DETR/PyTorch/build_env.sh
@@ -1,2 +1,3 @@
 conda create -n TQ_DETR_PyTorch python cython scipy pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.6 -c pytorch -c conda-forge -y
+conda activate TQ_DETR_PyTorch
 pip install YoungToolkit pycocotools
