50a160f80c6971fb39a518d44830e6ead7515690
AI-Jason-Young <AI.Jason.Young@outlook.com>
2023-07-20 16:40:27 +0800

    Update

---
+++ Experiments/DETR/PyTorch/get_models.sh
@@ -0,0 +1,3 @@
+wget -P bins https://dl.fbaipublicfiles.com/detr/detr-r101-2c7b67e5.pth
+wget -P bins/ https://dl.fbaipublicfiles.com/detr/detr-r101-dc5-a2e86def.pth
+wget -P bins https://download.pytorch.org/models/resnet101-63fe2227.pth
