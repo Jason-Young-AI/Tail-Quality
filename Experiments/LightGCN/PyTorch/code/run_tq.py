6a0980b79f7b72228c9b596ca0cffc5a7321b403
Zrsion <1079451538@qq.com>
2024-07-22 20:45:10 +0800

    update

--- Experiments/LightGCN/PyTorch/code/run_tq.py
+++ Experiments/LightGCN/PyTorch/code/run_tq.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
 from sklearn.mixture import GaussianMixture
 from scipy.spatial.distance import jensenshannon
+from typing import List
 
 import world
 import utils
@@ -105,6 +106,7 @@ def test_one_batch(X):
                 'precision':np.array(pre), 
                 'ndcg':np.array(ndcg)}
 
+
 def Inference(params):
     dataset = params['dataset']
     Recmodel = params['Recmodel']
@@ -122,96 +124,107 @@ def Inference(params):
     testDict: dict = dataset.testDict
     Recmodel: model.LightGCN
     # eval mode with no dropout
-    Recmodel = Recmodel.eval()
 
     max_K = max(world.topks)
-    with torch.no_grad():
-        users = list(testDict.keys())
-        try:
-            assert u_batch_size <= len(users) / 10
-        except AssertionError:
-            print(f"test_u_batch_size is too big for this dataset, try a small one {len(users) // 10}")
-        users_list = []
-        rating_list = []
-        groundTrue_list = []
-         
-        total_batch = len(users) // u_batch_size + 1
-        tmp_dic = dict()
-        for batch_id, batch_users in enumerate(utils.minibatch(users, batch_size=u_batch_size), start=1):
-            allPos = dataset.getUserPosItems(batch_users)
-            groundTrue = [testDict[u] for u in batch_users]
-            batch_users_gpu = torch.Tensor(batch_users).long()
-            batch_users_gpu = batch_users_gpu.to(world.device)
-
-            inference_start = time.perf_counter()
-            rating = Recmodel.getUsersRating(batch_users_gpu)
-            inference_end = time.perf_counter()
-            inference_time = inference_end - inference_start
-            tmp_dic[batch_id] = float(inference_time)
-            all_times.append(inference_time)
-
-            already_batch_number += 1
-            if already_run_number < warm_run: 
-                warm_batch_number += 1 
-            if already_run_number is not 0 and ((already_batch_number - warm_batch_number) % fit_batches) == 0:
-                fit_distribution_model = fit(all_times)
-                print(' len(fit_distribution_models) % window_size : ',len(fit_distribution_models) % window_size) 
-                if len(fit_distribution_models) % window_size == 0 and len(fit_distribution_models) is not 0:
-                    rjsd = check_fit_dynamic(fit_distribution_models, fit_distribution_model, all_times, window_size)
-                    rjsds.append(rjsd)
-                    sucess_flag = True if rjsd <= rJSD_threshold else False
-
-                fit_distribution_models.append(fit_distribution_model)
-                print('in inference, fit_distribution_models: ', fit_distribution_models)
-                
-            exclude_index = []
-            exclude_items = []
-            for range_i, items in enumerate(allPos):
-                exclude_index.extend([range_i] * len(items))
-                exclude_items.extend(items)
-            rating[exclude_index, exclude_items] = -(1<<10)
-            _, rating_K = torch.topk(rating, k=max_K)
-            rating = rating.cpu().numpy()
-            
-            del rating
-            users_list.append(batch_users)
-            rating_list.append(rating_K.cpu())
-            groundTrue_list.append(groundTrue)
-        assert total_batch == len(users_list)
-        
-        X = zip(rating_list, groundTrue_list)
-
-        pre_results = []
-        for batch_id, x in enumerate(X, start=1):
-            post_process_start = time.perf_counter()
-            pre_results.append(test_one_batch(x))
-            post_process_end = time.perf_counter()
-
-        if already_run_number == 0:
-            origin_quality = dict()   
-            for batch_id, result in enumerate(pre_results, start=1):
-                origin_quality[batch_id] = dict(
-                    recall = float(result['recall'].item()),
-                    precision = float(result['precision'].item()),
-                    ndcg = float(result['ndcg'].item())
-                )
-            
-            with open(results_basepath.joinpath('Light_GCN_Pytorch_origin_quality.json'), 'w') as f:
-                json.dump(origin_quality, f, indent=2)
-        already_run_number += 1
-        if fake_run:
-            return tmp_dic, params['already_run_number'], params['already_batch_number'], params['warm_batch_number'], params['fit_distribution_models'], params['rjsds'], params['sucess_flag']
-        else:
-            return tmp_dic, already_run_number, already_batch_number, warm_batch_number, fit_distribution_models, rjsds, sucess_flag
-        # for result in pre_results:
-        #     results['recall'] += result['recall']
-        #     results['precision'] += result['precision']
-        #     results['ndcg'] += result['ndcg']
-            
-        # results['recall'] /= float(len(users))
-        # results['precision'] /= float(len(users))
-        # results['ndcg'] /= float(len(users))
     
+    users = list(testDict.keys())
+    try:
+        assert u_batch_size <= len(users) / 10
+    except AssertionError:
+        print(f"test_u_batch_size is too big for this dataset, try a small one {len(users) // 10}")
+    users_list = []
+    rating_list = []
+    groundTrue_list = []
+        
+    total_batch = len(users) // u_batch_size + 1
+    tmp_dic = dict()
+    for batch_id, batch_users in enumerate(utils.minibatch(users, batch_size=u_batch_size), start=1):
+        allPos = dataset.getUserPosItems(batch_users)
+        groundTrue = [testDict[u] for u in batch_users]
+        batch_users_gpu = torch.Tensor(batch_users).long()
+        batch_users_gpu = batch_users_gpu.to(world.device)
+
+        inference_start = time.perf_counter()
+        rating = Recmodel.getUsersRating(batch_users_gpu)
+        inference_end = time.perf_counter()
+        inference_time = inference_end - inference_start
+        tmp_dic[batch_id] = float(inference_time)
+        all_times.append(inference_time)
+
+        already_batch_number += 1
+        if already_run_number < warm_run: 
+            warm_batch_number += 1 
+        if already_run_number is not 0 and ((already_batch_number - warm_batch_number) % fit_batches) == 0:
+            fit_distribution_model = fit(all_times)
+            print(' len(fit_distribution_models) % window_size : ', len(fit_distribution_models) % window_size) 
+            if len(fit_distribution_models) % window_size == 0 and len(fit_distribution_models) is not 0:
+                rjsd = check_fit_dynamic(fit_distribution_models, fit_distribution_model, all_times, window_size)
+                rjsds.append(rjsd)
+                print('During this run, rjsd is', rjsd)
+                sucess_flag = True if rjsd <= rJSD_threshold else False
+
+            fit_distribution_models.append(fit_distribution_model)
+            
+        exclude_index = []
+        exclude_items = []
+        for range_i, items in enumerate(allPos):
+            exclude_index.extend([range_i] * len(items))
+            exclude_items.extend(items)
+        rating[exclude_index, exclude_items] = -(1<<10)
+        _, rating_K = torch.topk(rating, k=max_K)
+        rating = rating.cpu().numpy()
+        
+        del rating
+        users_list.append(batch_users)
+        rating_list.append(rating_K.cpu())
+        groundTrue_list.append(groundTrue)
+    assert total_batch == len(users_list)
+    
+    X = zip(rating_list, groundTrue_list)
+
+    pre_results = []
+    for batch_id, x in enumerate(X, start=1):
+        post_process_start = time.perf_counter()
+        pre_results.append(test_one_batch(x))
+        post_process_end = time.perf_counter()
+
+    if already_run_number == 0:
+        origin_quality = dict()   
+        for batch_id, result in enumerate(pre_results, start=1):
+            origin_quality[batch_id] = dict(
+                recall = float(result['recall'].item()),
+                precision = float(result['precision'].item()),
+                ndcg = float(result['ndcg'].item())
+            )
+        
+        with open(results_basepath.joinpath('Light_GCN_Pytorch_origin_quality.json'), 'w') as f:
+            json.dump(origin_quality, f, indent=2)
+    already_run_number += 1
+    if fake_run:
+        return tmp_dic, params['already_run_number'], params['already_batch_number'], params['warm_batch_number'], params['fit_distribution_models'], params['rjsds'], params['sucess_flag']
+    else:
+        return tmp_dic, already_run_number, already_batch_number, warm_batch_number, fit_distribution_models, rjsds, sucess_flag
+    # for result in pre_results:
+    #     results['recall'] += result['recall']
+    #     results['precision'] += result['precision']
+    #     results['ndcg'] += result['ndcg']
+        
+    # results['recall'] /= float(len(users))
+    # results['precision'] /= float(len(users))
+    # results['ndcg'] /= float(len(users))
+
+
+def draw_rjsds(rjsds: List, results_basepath: pathlib.Path):
+    x_data = list(range(1, len(rjsds) + 1))
+    fig, ax = plt.subplots()
+    ax.plot(x_data, rjsds, marker='o', linestyle='-', color='b', label='rJSD')
+    ax.set_title('rJSD Fitting Progress')
+    ax.set_xlabel('Fitting Round')
+    ax.set_ylabel('rJSD')
+    ax.grid(True)
+    plt.savefig(results_basepath.joinpath("Light_GCN_Pytorch_rjsds.jpg"), format="jpg")
+    plt.savefig(results_basepath.joinpath("Light_GCN_Pytorch_rjsds.pdf"), format="pdf")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Multiple times on the Whole Test Dataset")
@@ -224,8 +237,8 @@ if __name__ == "__main__":
     parser.add_argument('--fake-run', type=bool, default=True) # To avoid the outliers processed during the first inference
     parser.add_argument('--window-size', type=int, default=5)
     parser.add_argument('--fit-batches', type=int, default=100)
-    parser.add_argument('--rJSD-threshold', type=float, default=0.005)
-
+    parser.add_argument('--rJSD-threshold', type=float, default=0.05)
+    parser.add_argument('--test-run', type=int, default=0)
     args = parser.parse_args()
 
     # dataset_path = pathlib.Path(args.dataset_path)
@@ -237,6 +250,7 @@ if __name__ == "__main__":
     fit_batches = args.fit_batches
     rJSD_threshold = args.rJSD_threshold
     fake_run = args.fake_run
+    test_run = args.test_run
 
     result_path = results_basepath.joinpath('Light_GCN_Pytorch.json')
     rjsds_path = results_basepath.joinpath('Light_GCN_Pytorch_rjsds.json')
@@ -246,25 +260,29 @@ if __name__ == "__main__":
     total_batches = 0
     if result_path.exists():
         with open (result_path, 'r') as f:
-            result_list = json.load(f)
-            for run in result_list:
+            results = json.load(f)
+            for run in results:
                 total_batches += len(run.keys())
-                for time in run.values():
-                    all_times.append(time)
+                for inference_time in run.values():
+                    all_times.append(inference_time)
+        with open(rjsds_path, 'r') as f:
+            rjsds = json.load(f)
+        with open(fit_distribution_path, 'rb') as f:
+            fit_distribution_models = pickle.load(f)
     
-        already_run_number = len(result_list)
+        already_run_number = len(results)
         already_batch_number = total_batches 
-        warm_batch_number = len(result_list[0].keys())
+        warm_batch_number = len(results[0].keys())
     else:
-        result_list = list()
+        results = list()
+        fit_distribution_models = list()
+        rjsds = list()
         already_run_number = 0
         already_batch_number = 0
         warm_batch_number = 0
-    
-    fit_distribution_models = list()
-    rjsds = list()
+
     """
-        result_list = [
+        results = [
             { 
                 "batch_1" : float,
                 ...
@@ -284,42 +302,61 @@ if __name__ == "__main__":
     print(f"load and save to {weight_file}")
     if world.LOAD:
         try:
-            Recmodel.load_state_dict(torch.load(weight_file,map_location=torch.device('cpu')))
+            Recmodel.load_state_dict(torch.load(weight_file, map_location=torch.device('cuda')))
             print(f"loaded model weights from {weight_file}")
         except FileNotFoundError:
             print(f"{weight_file} not exists, start from beginning")
     sucess_flag = False
+    Recmodel.eval()
+    loop = 0 # for debugging
 
-    while not sucess_flag:
-        params = {
-            'dataset': dataset,
-            'Recmodel': Recmodel,
-            'fake_run': fake_run,
-            'warm_run': warm_run,
-            'already_run_number': already_run_number,
-            'already_batch_number': already_batch_number,
-            'warm_batch_number': warm_batch_number,
-            'fit_distribution_models': fit_distribution_models,
-            'rjsds': rjsds,
-            'rJSD_threshold': rJSD_threshold,
-            'sucess_flag': sucess_flag
-        }
-        result, already_run_number, already_batch_number, warm_batch_number, fit_distribution_models, rjsds, sucess_flag = Inference(params)
-        if not fake_run:
-            result_list.append(result)
-        fake_run = False
+    with torch.no_grad():
+        while not sucess_flag:
+            loop += 1 # for debugging
+            params = {
+                'dataset': dataset,
+                'Recmodel': Recmodel,
+                'fake_run': fake_run,
+                'warm_run': warm_run,
+                'already_run_number': already_run_number,
+                'already_batch_number': already_batch_number,
+                'warm_batch_number': warm_batch_number,
+                'fit_distribution_models': fit_distribution_models,
+                'rjsds': rjsds,
+                'rJSD_threshold': rJSD_threshold,
+                'sucess_flag': sucess_flag
+            }
 
-        print(f'in run {already_run_number} :')
-        print('already_batch_number: ', already_batch_number)
-        print('warm_batch_number: ', warm_batch_number)
-        print('fitted_models: ', len(fit_distribution_models))
-        print('rjsds:', len(rjsds))
+            print('-------before_inference-------')
+            print(f'in loop {loop}:', loop)
+            print(f'in run {already_run_number} :')
+            print('already_batch_number: ', already_batch_number)
+            print('warm_batch_number: ', warm_batch_number)
+            print('fitted_models: ', len(fit_distribution_models))
+            print('rjsds:', len(rjsds))
+
+            result, already_run_number, already_batch_number, warm_batch_number, \
+            fit_distribution_models, rjsds, sucess_flag = Inference(params)
+            if not fake_run:
+                results.append(result)
+            fake_run = False
+            
+            print('-------after_inference-------')
+            print(f'in loop {loop}:', loop)
+            print(f'in already_run_number: {already_run_number}')
+            print('already_batch_number: ', already_batch_number)
+            print('warm_batch_number: ', warm_batch_number)
+            print('fitted_models: ', len(fit_distribution_models))
+            print('rjsds_nums:', len(rjsds))
+            if already_run_number == test_run:
+                break 
 
     with open(result_path, 'w') as f:
-        json.dump(result_list, f, indent=2)
+        json.dump(results, f, indent=2)
     with open(rjsds_path, 'w') as f:
         json.dump(rjsds, f, indent=2)
-    with open(fit_distribution_path, 'w') as f:
-        pickle.dump(fit_distribution_models, f) 
+    with open(fit_distribution_path, 'wb') as f:
+        pickle.dump(fit_distribution_models, f)
+    draw_rjsds(rjsds, results_basepath) 
     
 
