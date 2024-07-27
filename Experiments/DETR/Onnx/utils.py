325465be32e30aded5b7a562699d33c39443339c
Zrsion <1079451538@qq.com>
2024-07-27 10:03:59 +0800

    update onnx

---
+++ Experiments/DETR/Onnx/utils.py
@@ -0,0 +1,31 @@
+import torch
+
+
+def convert_to_xywh(boxes):
+    xmin, ymin, xmax, ymax = boxes.unbind(1)
+    return torch.stack((xmin, ymin, xmax - xmin, ymax - ymin), dim=1)
+
+
+def prepare_for_coco_detection(raw_results, image_indices):
+    coco_results = []
+    for prediction, image_index in zip(raw_results, image_indices):
+        if len(prediction) == 0:
+            continue
+
+        boxes = prediction["boxes"]
+        boxes = convert_to_xywh(boxes).tolist()
+        scores = prediction["scores"].tolist()
+        labels = prediction["labels"].tolist()
+
+        coco_results.append(
+            [
+                {
+                    "image_id": image_index,
+                    "category_id": labels[k],
+                    "bbox": box,
+                    "score": scores[k],
+                }
+                for k, box in enumerate(boxes)
+            ]
+        )
+    return coco_results
