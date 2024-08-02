fa6c91bc6ba0f6b0213181ad3e2999e8ef54a2b7
Jason Young <AI.Jason.Young@outlook.com>
2024-08-02 22:09:04 +0800

    Update build_env.sh

--- Experiments/MobileNet/ONNX/build_env.sh
+++ Experiments/MobileNet/ONNX/build_env.sh
@@ -3,7 +3,7 @@ source ./vars.sh
 conda create -n ${THIS_ENV_NAME} python=3.8 -y
 source $CONDA_PREFIX/bin/activate ${THIS_ENV_NAME}
 pip install future --no-input
-conda install pytorch==1.5.1 torchvision==0.6.1 cudatoolkit=9.2 -c pytorch -y
+conda install cudatoolkit=9.2 -c pytorch -y
 pip install onnx requests==2.20 --no-input
 pip install mxnet-cu92mkl --pre -U --no-input
 pip install tqdm --no-input
