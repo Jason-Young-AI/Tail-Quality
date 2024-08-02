aa503c15417807c1314cf6696f950b2b8ef3746f
Zrsion <1079451538@qq.com>
2024-08-02 19:43:57 +0800

    update

--- Visualization/TQ/draw_tq.sh
+++ Visualization/TQ/draw_tq.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 python draw_tq.py \
- --results-basepath /younger/peng/TQResults/MobileNet_Pytorch \
+ --results-basepath ../../../TQResults/MobileNet_Pytorch \
  --metric-type acc \
  --time-type inference \
  --worker-number 24 \
  --interplt-num 10000
 
 python draw_tq.py \
- --results-basepath /younger/peng/TQResults/MobileNet_Pytorch \
+ --results-basepath ../../../TQResults/MobileNet_Pytorch \
  --metric-type acc \
  --time-type total \
  --worker-number 24 \
