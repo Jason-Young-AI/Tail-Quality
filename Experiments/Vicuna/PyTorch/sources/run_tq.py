532ee17d3b38e84c35ae7bc5bd27063d8e3f1782
Jason-Young-AI <AI.Jason.Young@outlook.com>
2024-08-04 21:28:04 +0800

    Update

--- Experiments/Vicuna/PyTorch/sources/run_tq.py
+++ Experiments/Vicuna/PyTorch/sources/run_tq.py
@@ -354,11 +354,16 @@ if __name__ == "__main__":
     parser.add_argument('--batch-size', type=int, default=1)
 
     parser.add_argument('--only-quality', action='store_true')
-    parser.add_argument('--golden-path', type=str)
-    parser.add_argument('--result-path', type=str)
-    parser.add_argument('--others-path', type=str)
+    parser.add_argument('--golden-path', type=str, default=None)
+    parser.add_argument('--result-path', type=str, default=None)
+    parser.add_argument('--others-path', type=str, default=None)
 
     args = parser.parse_args()
+
+    if args.only_quality:
+        assert args.golden_path is not None
+        assert args.result_path is not None
+        assert args.others_path is not None
 
     if args.model_path == 'lmsys/vicuna-13b-v1.3':
         print(f"Using HuggingFace [Online] Pretrained Model: \'lmsys/vicuna-13b-v1.3\'")
