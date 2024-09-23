d6401cfe145e9db8ab219249f0a6a78867c1423d
Jason Young <AI.Jason.Young@outlook.com>
2024-09-23 23:04:40 +0800

    Update build_env.sh

--- Experiments/MobileNet/PyTorch/build_env.sh
+++ Experiments/MobileNet/PyTorch/build_env.sh
@@ -6,4 +6,4 @@ conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvi
 #conda install pytorch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1 cudatoolkit=10.2 -c pytorch -y
 #conda install pytorch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 pytorch-cuda=11.7 -c pytorch -c nvidia -y
 pip install tqdm --no-input
-pip install numpy matplotlib scikit-learn scipy tqdm KDEpy --no-input
+pip install numpy==1.26.4 matplotlib scikit-learn scipy tqdm KDEpy --no-input
