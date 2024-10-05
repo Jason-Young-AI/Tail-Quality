6a5a6345a5c7a6a07044f0f099112fe5b762cd59
AI-Jason-Young <AI.Jason.Young@outlook.com>
2024-10-05 12:03:26 +0800

    Update

--- Evaluation/calculate_cg.py
+++ Evaluation/calculate_cg.py
@@ -1,40 +1,17 @@
-import sys
-import numpy
 import pathlib
 import argparse
 
-from .metrics              import Task
-from .metrics.detr         import DETR
-from .metrics.vicuna       import Vicuna
-from .metrics.light_gcn    import LightGCN
-from .metrics.hybrid_nets  import HybridNets
-from .metrics.mobile_net   import MobileNet
-from .metrics.emotion_flow import EmotionFlow
-
-from .metrics.mobile_net_faster   import MobileNetFaster
-from .metrics.light_gcn_faster    import LightGCNFaster
-
 from .utils.io import save_pickle, load_pickle
-from .utils.times import remove_outliers, get_outlier_flags
-from .tail_quality import tail_quality
-from .calculate_tl import get_tail_latency
 
 
-tasks: dict[str, Task] = dict(
-    detr         = DETR,
-    vicuna       = Vicuna,
-    light_gcn    = LightGCNFaster,
-    hybrid_nets  = HybridNets,
-    mobile_net   = MobileNetFaster,
-    emotion_flow = EmotionFlow,
-)
 
-
-def swipe_all_times(rjsd_stop: float, all_rjsds: dict[str, list[int]], all_times: dict[str, list[dict[int, float]]], fit_run_number: int, window_size: int, warm_run: int) -> tuple[list[dict[int, float]], list[dict[int, float]]]:
+def swipe(rjsd_stop: float, all_rjsds: dict[str, list[int]], all_times: dict[str, list[dict[int, float]]], fit_run_number: int, window_size: int, warm_run: int) -> dict:
     fit_distribution_number = 0
 
     swiped_inference_all_times = list()
+    swiped_inference_all_rjsds = list()
     swiped_total_all_times = list()
+    swiped_total_all_rjsds = list()
 
     inference_all_times = all_times['inference']
     total_all_times = all_times['total']
@@ -61,18 +38,27 @@ def swipe_all_times(rjsd_stop: float, all_rjsds: dict[str, list[int]], all_times
         total_yes = False
         total_rjsd_converge = True
 
+    inference_converge_round = 0
+    total_converge_round = 0
     for already_run, (inference_times, total_times) in enumerate(zip(inference_all_times, total_all_times), start=1):
         if inference_rjsd_converge and total_rjsd_converge:
             break
 
         if not inference_rjsd_converge:
             swiped_inference_all_times.append(inference_times)
+            inference_converge_round += 1
         if not total_rjsd_converge:
             swiped_total_all_times.append(total_times)
+            total_converge_round += 1
 
         if already_run > warm_run and (already_run - warm_run) % fit_run_number == 0:
-            inference_rjsd = all_rjsds['inference'][fit_distribution_number]
-            total_rjsd = all_rjsds['total'][fit_distribution_number]
+            if not inference_rjsd_converge:
+                inference_rjsd = all_rjsds['inference'][fit_distribution_number]
+                swiped_inference_all_rjsds.append(inference_rjsd)
+            if not total_rjsd_converge:
+                total_rjsd = all_rjsds['total'][fit_distribution_number]
+                swiped_total_all_rjsds.append(total_rjsd)
+
             if fit_distribution_number % window_size == 0 and fit_distribution_number != 0:
                 if not inference_rjsd_converge and inference_rjsd <= rjsd_stop:
                     inference_rjsd_converge = True
@@ -83,7 +69,16 @@ def swipe_all_times(rjsd_stop: float, all_rjsds: dict[str, list[int]], all_times
             if fit_distribution_number >= len(all_rjsds['inference']):
                 break
 
-    return inference_yes, swiped_inference_all_times, total_yes, swiped_total_all_times
+    return dict(
+        inference_converge=inference_yes,
+        inference_converge_round=inference_converge_round,
+        swiped_inference_all_rjsds=swiped_inference_all_rjsds,
+        swiped_inference_all_times=swiped_inference_all_times,
+        total_converge=total_yes,
+        total_converge_round=total_converge_round,
+        swiped_total_all_rjsds=swiped_total_all_rjsds,
+        swiped_total_all_times=swiped_total_all_times
+    )
 
 
 
@@ -112,19 +107,16 @@ if __name__ == '__main__':
 
     print(f'This I Dir Name: {ip_dirpath}')
     print(f'This O Dir Name: {op_dirpath}')
-    rjsd_stop_s = list()
-    rjsd_stop_i = list()
+    rjsd_stop_detail = list()
     for rjsd_stop in rjsd_stops:
         print(f' -> RJSD = {rjsd_stop}')
         all_rjsds = load_pickle(ip_dirpath.joinpath('All_rJSDs.pickle'))
         all_times = load_pickle(ip_dirpath.joinpath('All_Times.pickle'))
-        inference_yes, swiped_inference_all_times, total_yes, swiped_total_all_times = swipe_all_times(rjsd_stop, all_rjsds, all_times, fit_run_number, window_size, warm_run)
-        rjsd_stop_s.append((rjsd_stop, dict(inference_yes=inference_yes, inference=swiped_inference_all_times, total_yes=total_yes, total=swiped_total_all_times)))
-        rjsd_stop_i.append((rjsd_stop, dict(inference_yes=inference_yes, inference=len(swiped_inference_all_times), total_yes=total_yes, total=len(swiped_total_all_times))))
-        if inference_yes:
-            print(f'    Inference - {len(swiped_inference_all_times)}')
-        if total_yes:
-            print(f'    Total - {len(swiped_total_all_times)}')
-    save_pickle(rjsd_stop_s, op_dirpath.joinpath('rjsd2time_s.pickle'))
-    save_pickle(rjsd_stop_i, op_dirpath.joinpath('rjsd2time_i.pickle'))
+        swiped = swipe(rjsd_stop, all_rjsds, all_times, fit_run_number, window_size, warm_run)
+        rjsd_stop_detail.append((rjsd_stop, swiped))
+        if swiped['inference_converge']:
+            print(f'    Inference - {swiped["inference_converge_round"]}')
+        if swiped['total_converge']:
+            print(f'    Total - {swiped["total_converge_round"]}')
+    save_pickle(rjsd_stop_detail, op_dirpath.joinpath('rjsd_stop_detail.pickle'))
     print(f'This Done')
