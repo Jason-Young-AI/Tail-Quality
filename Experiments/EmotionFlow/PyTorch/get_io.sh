7f11f655fbcd8df83e28968a026ef458d1bb2d13
AI-Jason-Young <AI.Jason.Young@outlook.com>
2024-10-05 12:46:42 +0800

    Just Output IO size. All The Same

---
+++ Experiments/EmotionFlow/PyTorch/get_io.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./vars.sh
+
+source $CONDA_PREFIX/bin/activate ${THIS_ENV_NAME}
+
+cd ./sources
+
+python get_io.py \
+  --results-basepath ${THIS_RESULTS_DIR} \
+  --warm-run 5 \
+  --window-size 5 \
+  --fit-run-number 2 \
+  --rJSD-threshold 0.02 \
+  --max-run 1000000 \
+  --dataset-path ${THIS_DATASET_DIR} \
+  --model-path ${THIS_MODEL_DIR}/${THIS_MODEL_FILENAME} \
+  --only-quality \
+  --golden-path ${THIS_RESULTS_DIR}/golden.json \
+  --result-path ${THIS_RESULTS_DIR}/result.json \
+  --io-path ${THIS_RESULTS_DIR}/io.json
+
+cd ..
