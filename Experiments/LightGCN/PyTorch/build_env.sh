02b593f6757498f988ecfdc9e4ffeacac618481d
AI-Jason-Young <AI.Jason.Young@outlook.com>
2024-10-05 13:32:30 +0800

    Just Output IO size. All The Same

--- Experiments/LightGCN/PyTorch/build_env.sh
+++ Experiments/LightGCN/PyTorch/build_env.sh
@@ -2,6 +2,7 @@
 source ./vars.sh
 conda create -n ${THIS_ENV_NAME} python=3.10 -y
 source $CONDA_PREFIX/bin/activate ${THIS_ENV_NAME}
-conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+# conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu118
 pip install tensorboardX transformers vocab pandas scipy pycocotools scikit-learn tqdm --no-input
 pip install YoungToolkit matplotlib KDEpy --no-input
